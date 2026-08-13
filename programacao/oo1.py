bf71f75ce043c70c03a96655e2c7ba91b9bd0f3a
Victor do Carmo Medeiros <168451171+victorcarmomedeiros@users.noreply.github.com>
2026-08-13 09:04:02 -0300

    Add files via upload

---
+++ programacao/oo1.py
@@ -0,0 +1,11 @@
+class pessoa :
+    nome == ''
+    Cpf == ''
+    idade == 0
+
+
+    def caminhar(self):
+        print("pessoa caminhando")
+
+    def correr(self):
+        print("pessoa correndo")    
